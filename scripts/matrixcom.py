413fd0719d05814a4b1c5e22f46ab6f3bac9a11e
Leon Teichroeb <bigleon00@gmail.com>
2025-05-18 21:31:20 +0200

    Website overhaul

--- scripts/matrixcom.py
+++ scripts/matrixcom.py
@@ -7,49 +7,60 @@ class NotConnected(Exception):
     pass
 
 
-def connect_to_matrix(host, port):
-    print("[matrixcom] Connecting to {}:{}".format(host, port))
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(0.5)  #Wait upto 0.5 seconds for a server response
-    try:
-        sock.connect((host, port))
-    except socket.gaierror:
-        pass
-    except TimeoutError:
-        raise NotConnected
+class CommandError(Exception):
+    pass
+
+
+class MatrixConnection:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
     
-    return sock
 
-
-def _receive_response(clientSocket):
-    message = ""
-    command_response = ""
-    while True:
+    def _connect(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)  #Wait upto 0.5 seconds for a server response
         try:
-            data = clientSocket.recv(2048)
-        except socket.timeout:
-            return False
-
-        if data == 0:
-            return False
-
-        data = data.decode()
-
-        tokens = data.split(';')
-        for token in tokens[:-1]:
-            message += token
-            if message == "ack":
-                return command_response
-            else:
-                command_response = message.rstrip()
-            message = ''
-
-        message += tokens[-1]  #If it was ended by the delimeter it will be empty
+            sock.connect((self.host, self.port))
+            print("[backend] Connected to {}:{}".format(self.host, self.port))
+        except socket.gaierror:
+            pass
+        except (TimeoutError, OSError):
+            print("[backend] Failed to connect to {}:{}".format(self.host, self.port))
+            raise NotConnected
+        return sock
 
 
-def send_command(sock, command):
-    if not sock:
-        raise NotConnected
+    def send_command(self, command):
+        sock = self._connect()
+        sock.sendall(str.encode('%'.join(command) + ';'))
+        return self._receive_response(sock)
 
-    sock.sendall(str.encode('%'.join(command) + ';'))
-    return _receive_response(sock)
+
+    def _receive_response(self, sock):
+        message = ""
+        command_response = ""
+        while True:
+            try:
+                data = sock.recv(2048)
+            except socket.timeout:
+                return False
+
+            if data == 0:
+                return False
+
+            data = data.decode()
+
+            tokens = data.split(';')
+            for token in tokens[:-1]:
+                message += token
+                if message == "ack":
+                    print(f"[backend] Received {tokens}")
+                    if command_response.startswith("[Error]"):
+                        raise CommandError
+                    return command_response
+                else:
+                    command_response = message.rstrip()
+                message = ''
+
+            message += tokens[-1]  #If it was ended by the delimeter it will be empty
--- wsgi.py
+++ wsgi.py
@@ -1,89 +1,148 @@
-from flask import Flask, render_template, redirect, g
-import scripts.matrixcom as matrixcom
+from flask import Flask, render_template, redirect, url_for, request
+from scripts.matrixcom import MatrixConnection, NotConnected
 
-
-host="192.168.0.5"
+host="192.168.178.5"
 port=8252
 
-sock = None
-effects_index = 0
-effects = []
 
-
-def prepare_socket():
-    global sock
-    if sock is None:
-        sock = matrixcom.connect_to_matrix("192.168.0.5", 8252)
-
-
-def get_matrixcom_effects():
-    global effects
-    # Can raise a NotConnected exception
-    prepare_socket()
-    effects = matrixcom.send_command(sock, ['apps'])
+def get_matrixcom_effects(conn: MatrixConnection) -> list[str]:
+    """ Returns a list of effects supported by the LED matrix. """
+    effects = conn.send_command(['apps'])
     return [e[3:] for e in effects.split('\n')]
 
 
+def get_app_settings(conn: MatrixConnection, app: str) -> dict[str, bool | float | int | str]:
+    """ Returns a dictionary of properties and their value for `app`. """
+    all_settings = conn.send_command(['list'])
+    curr_app = None
+    settings = {}
+    for line in map(str.strip, all_settings.split('\n')):
+        if line.startswith('-'):
+            if curr_app == app:
+                setting, value = line[1:].split(': ')
+                settings[setting] = value
+        else:
+            curr_app = line
+    
+    for k, v in settings.items():
+        if ',' in v:
+            # This is a color. It has four floats for RGBW.
+            settings[k] = list(map(float, v.split(',')))
+        try:
+            v_int = int(v)
+            settings[k] = v_int
+        except ValueError:
+            try:
+                v_float = float(v)
+                settings[k] = v_float
+            except ValueError:
+                # Keep it as a string property.
+                pass
+    return settings
+
+
+def set_app_setting(conn: MatrixConnection, setting: str, value):
+    """ Sets the setting to the provided value. """
+    cmd = ['set', setting, str(value)]
+    print(f"Sending command {cmd}")
+    conn.send_command(cmd)
+
+
 def create_app():
-    global effects, effects_index
     app = Flask(__name__)
 
-    effects_index = 0
-    effects = get_matrixcom_effects()
+    # This is where we connect to the LED matrix. We keep this connection persistent.
+    conn = MatrixConnection(host, port)
+    effects = None
+    active_effect = None
 
     @app.route("/")
     def home():
-        return render_template('index.html', effect_name=effects[effects_index])
-
-
-    @app.route("/next")
-    def next():
-        global sock, effects, effects_index
-        effects_index += 1
-        if effects_index >= len(effects):
-            effects_index = 0
-        new_effect = effects[effects_index]
+        nonlocal effects
+        get_app_settings(conn, "colorwave")
         try:
-            prepare_socket()
-            matrixcom.send_command(sock, ['start', new_effect])
-        except matrixcom.NotConnected:
-            return redirect("/disconnected")
-        return redirect("/")
+            # Try to use cached effects to serve the page faster.
+            if effects is None:
+                effects = get_matrixcom_effects(conn)
+            brightness = get_app_settings(conn, "matrix")["brightness"]
+            return render_template(
+                'index.html',
+                no_connection=False,
+                active_effect=active_effect,
+                effects=effects,
+                brightness=brightness
+            )
+        except NotConnected:
+            effects = None
+            return render_template(
+                'index.html',
+                no_connection=True
+            )
 
-
-    @app.route("/previous")
-    def previous():
-        global sock, effects, effects_index
-        effects_index -= 1
-        if effects_index < 0:
-            effects_index = len(effects) - 1
-        new_effect = effects[effects_index]
+    @app.route("/toggle/<effect>")
+    def toggle_effect(effect):
+        nonlocal active_effect, effects
         try:
-            prepare_socket()
-            matrixcom.send_command(sock, ['start', new_effect])
-        except matrixcom.NotConnected:
-            return redirect("/disconnected")
-        return redirect("/")
+            if effect == active_effect:
+                conn.send_command(['stop', effect])
+                active_effect = None
+            else:
+                conn.send_command(['start', effect])
+                active_effect = effect
+        except NotConnected:
+            effects = None
+            active_effect = None
+        return redirect(url_for('home'))
 
+    @app.route("/settings/<effect>", methods=["GET", "POST"])
+    def effect_settings(effect):
+        nonlocal effects
+        if request.method == "POST":
+            for key, value in request.form.items():
+                # Try to parse numbers, else treat as string
+                try:
+                    if "," in value:
+                        # RGBW float list
+                        parsed = list(map(float, value.split(',')))
+                        value = ','.join(map(str, parsed))
+                    elif "." in value:
+                        value = float(value)
+                    else:
+                        value = int(value)
+                except ValueError:
+                    value = value.strip()
+                
+                setting = f"matrix.{effect}.{key}"
+                try:
+                    set_app_setting(conn, setting, value)
+                except NotConnected:
+                    effects = None
+                    return redirect(url_for('home'))
 
-    @app.route("/stop")
-    def stop():
-        global sock
+            return redirect(url_for("effect_settings", effect=effect))
+
         try:
-            prepare_socket()
-            matrixcom.send_command(sock, ['stop'])
-        except matrixcom.NotConnected:
-            return redirect("/disconnected")
-        return redirect("/")
+            settings = get_app_settings(conn, effect)
+            return render_template("settings.html", effect=effect, settings=settings)
+        except NotConnected:
+            effects = None
+            return redirect(url_for('home'))
 
-
-    @app.route("/disconnected")
-    def disconnected():
+    @app.route("/set_brightness", methods=["POST"])
+    def set_brightness():
+        nonlocal effects
+        brightness = request.form.get("brightness")
+        value = float(brightness)
+        if value < 0.0:
+            value = 0.0
+        elif value > 255.0:
+            value = 255.0
+        
         try:
-            prepare_socket()
-            return redirect("/")
-        except matrixcom.NotConnected:
-            pass
-        return render_template('disconnected.html')
+            set_app_setting(conn, "matrix.brightness", value)
+        except NotConnected:
+            effects = None
+            return redirect(url_for('home'))
+        return '', 204  # No content
 
     return app
